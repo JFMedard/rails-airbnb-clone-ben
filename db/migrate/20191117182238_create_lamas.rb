a24446fc461473c14a1de638326951071600da92
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-11-18 14:10:49 +0100

    Lama model final version to be validated

--- db/migrate/20191117182238_create_lamas.rb
+++ db/migrate/20191117182238_create_lamas.rb
@@ -4,9 +4,11 @@ class CreateLamas < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :description
       t.float :price
+      t.string :address
       t.boolean :is_available
       t.integer :capacity
       t.boolean :driver
+      t.string :photo
       t.references :user, foreign_key: true
 
       t.timestamps
