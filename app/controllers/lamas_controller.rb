7948a326152554b39dd195be72fcef5a0a8ad19b
Diana Capunay <diana.capunay-jara@hotmail.com>
2019-11-18 15:12:16 +0100

    lamas controller methods created

--- app/controllers/lamas_controller.rb
+++ app/controllers/lamas_controller.rb
@@ -1,2 +1,44 @@
 class LamasController < ApplicationController
+  def index
+    @lamas = Lama.all
+  end
+
+  def new
+    @lama = Lama.new
+  end
+
+  def create
+    @lama = Lama.new(lama_params)
+    if @lama.save
+      redirect_to lamas_path(@lamas)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @lama = Lama.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def edit
+    @lama = Lama.find(params[:id])
+  end
+
+  def update
+    @lama = Lama.find(params[:id])
+    @lama.update(lama_params)
+    redirect_to lamas_path(@lamas)
+  end
+
+  def destroy
+    @lama = Lama.find(params[:id])
+    @lama.destroy
+    redirect_to lamas_path(@lamas)
+  end
+
+  private
+  def lama_params
+    params.require(:lama).permit(:title, :description, :price, :capacity)
+  end
 end
