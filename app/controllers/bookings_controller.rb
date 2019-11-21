f989a5334fe902e83ab6c9ea85305e426b71477a
Carla Dessommes <carladessommes@gmail.com>
2019-11-21 15:04:36 +0100

    user can cancel bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -32,6 +32,12 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_bookings_path
+  end
+
   private
 
   def set_booking
